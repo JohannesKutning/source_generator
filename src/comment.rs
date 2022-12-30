cbcf1415931255ab79b481aa6592df0b9d82ae1a
Johannes Kutning <120175758+JohannesKutning@users.noreply.github.com>
2022-12-30 15:09:23 +0100

    Adds use statements to comment
    This shortens the usage of SingleLineComment and MultiLineComment.

--- src/comment.rs
+++ src/comment.rs
@@ -1,3 +1,6 @@
 pub mod single_line_comment;
 pub mod multi_line_comment;
 
+pub use crate::comment::single_line_comment::SingleLineComment as SingleLineComment;
+pub use crate::comment::multi_line_comment::MultiLineComment as MultiLineComment;
+
