e8de4e435c10da4b998d64796e14cfa9bbfa98ec
Johannes Kutning <120175758+JohannesKutning@users.noreply.github.com>
2023-01-17 15:49:45 +0100

    Adds deserilisation of port with tests

--- src/vhdl/port.rs
+++ src/vhdl/port.rs
@@ -1,10 +1,13 @@
+use serde_derive::Deserialize;
 use crate::vhdl::direction::Direction;
 use crate::element::Element;
 
+#[derive(Deserialize, Debug, Clone)]
 pub struct Port {
     name : String,
     direction : Direction,
     data_type : String,
+    #[serde(default)]
     default : String
 }
 
@@ -20,7 +23,14 @@ impl Port {
             default : & str ) -> Port  {
         Port{ name : name.to_string(), direction : direction, data_type : data_type.to_string(),
                 default : default.to_string() }
+    }
 
+    pub fn from_json( _description : & str ) {
+    }
+
+    pub fn clone_invert( & self ) -> Port {
+        Port::new_with_default( & self.name, self.direction.invert(), & self.data_type,
+            & self.default )
     }
 
     pub fn get_name( & self ) -> & String {
@@ -29,10 +39,6 @@ impl Port {
 
     pub fn set_name( & mut self, name : & str ) {
         self.name = name.to_string();
-    }
-
-    pub fn invert( & self ) -> Port {
-        Port::new_with_default( & self.name, self.direction.invert(), & self.data_type, & self.default )
     }
 }
 
@@ -55,44 +61,64 @@ impl Element for Port {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::error::Error;
+
+    const INPUT : &'static str = "test : in boolean";
+    const INPUT_WITH_DEFAULT : &'static str = "test : in boolean := true";
+    const OUTPUT : &'static str = "test : out boolean";
+    const INOUT : &'static str = "test : inout boolean";
+    const BUFFER : &'static str = "test : buffer boolean";
 
     #[test]
     fn input_port() {
         let port = Port::new( "test", Direction::IN, "boolean" );
-
-        assert_eq!(
-            port.to_source_code( 0 ),
-            String::from( "test : in boolean" )
-        );
+        assert_eq!( port.to_source_code( 0 ), INPUT.to_string() );
     }
 
     #[test]
     fn output_port() {
         let port = Port::new( "test", Direction::OUT, "boolean" );
-
-        assert_eq!(
-            port.to_source_code( 0 ),
-            String::from( "test : out boolean" )
-        );
+        assert_eq!( port.to_source_code( 0 ), OUTPUT.to_string() );
     }
 
     #[test]
     fn inout_port() {
         let port = Port::new( "test", Direction::INOUT, "boolean" );
-
-        assert_eq!(
-            port.to_source_code( 0 ),
-            String::from( "test : inout boolean" )
-        );
+        assert_eq!( port.to_source_code( 0 ), INOUT.to_string() );
     }
 
     #[test]
     fn buffer_port() {
         let port = Port::new( "test", Direction::BUFFER, "boolean" );
+        assert_eq!( port.to_source_code( 0 ), BUFFER.to_string() );
+    }
 
-        assert_eq!(
-            port.to_source_code( 0 ),
-            String::from( "test : buffer boolean" )
-        );
+    #[test]
+    fn input_port_with_default() {
+        let port = Port::new_with_default( "test", Direction::IN, "boolean", "true" );
+        assert_eq!( port.to_source_code( 0 ), INPUT_WITH_DEFAULT.to_string() );
+    }
+
+    #[test]
+    fn deserialize() -> Result< (), Box< dyn Error > > {
+        let port : Port = serde_json::from_str(
+            "{\"name\" : \"test\", \"direction\" : \"in\", \"data_type\" : \"boolean\"}" )?;
+        assert_eq!( port.to_source_code( 0 ), INPUT.to_string() );
+        Ok(())
+    }
+
+    #[test]
+    fn deserialize_with_default() -> Result< (), Box< dyn Error > > {
+        let port : Port = serde_json::from_str(
+            "{\"name\" : \"test\", \"direction\" : \"in\", \"data_type\" : \"boolean\", \"default\" : \"true\"}" )?;
+        assert_eq!( port.to_source_code( 0 ), INPUT_WITH_DEFAULT.to_string() );
+        Ok(())
+    }
+
+    #[test]
+    fn deserialize_invalid() {
+        let ret : Result< Port, serde_json::Error > = serde_json::from_str( "\"invalid\"" );
+        assert!( ret.is_err() );
     }
 }
+
